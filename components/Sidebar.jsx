1d3f2b3f1c6deaa1926a85b97eda585128d48abc
Bralex-svg <alexadam269@gmail.com>
2022-11-22 11:41:43 -0800

    added few pages

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import React from "react";
-
+import { CiUser } from "react-icons/ci";
+import { AiOutlineIdcard } from "react-icons/ai";
 export default function Sidebar() {
   return (
     <>
@@ -63,46 +65,20 @@ export default function Sidebar() {
                   </a>
                 </li>
                 <li className="rounded-sm">
-                  <a
-                    href="#"
+                  <Link
+                    href="/Patients/newPatient"
                     className="flex items-center p-2 space-x-3 rounded-md"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-gray-100"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                      />
-                    </svg>
+                    <CiUser className="text-white text-2xl" />
                     <span className="text-gray-100">Patients</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="rounded-sm">
                   <a
                     href="#"
                     className="flex items-center p-2 space-x-3 rounded-md"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-gray-100"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                      />
-                    </svg>
+                    <AiOutlineIdcard className="text-white text-2xl" />
                     <span className="text-gray-100">ID CARDS</span>
                   </a>
                 </li>
@@ -159,37 +135,6 @@ export default function Sidebar() {
             </div>
           </div>
         </div>
-        <div className="container mx-auto mt-12">
-          <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3">
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-              <div className="text-sm font-medium text-gray-500 truncate">
-                Total users
-              </div>
-              <div className="mt-1 text-3xl font-semibold text-gray-900">
-                12,00
-              </div>
-            </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-              <div className="text-sm font-medium text-gray-500 truncate">
-                Total Profit
-              </div>
-              <div className="mt-1 text-3xl font-semibold text-gray-900">
-                $ 450k
-              </div>
-            </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-              <div className="text-sm font-medium text-gray-500 truncate">
-                Total Orders
-              </div>
-              <div className="mt-1 text-3xl font-semibold text-gray-900">
-                20k
-              </div>
-            </div>
-          </div>
-        </div>
-        <h1 className="mt-[20%] font-bold text-3xl  -translate-x-[250%] ">
-          UNDER CONSTRUCTION...
-        </h1>
       </div>
     </>
   );
---
+++ pages/Patients/newPatient.jsx
@@ -0,0 +1,13 @@
+import Patient from "../../components/Patients";
+import Sidebar from "../../components/Sidebar";
+
+export default function NewPatient() {
+  return (
+    <>
+      <div>
+        <Sidebar />
+        <Patient />
+      </div>
+    </>
+  );
+}
